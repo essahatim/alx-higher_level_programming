b07ee770a39053f40741d6e37480144b446e2664
essahatim <hatimessabagh@gmail.com>
2023-11-28 15:29:36 -0800

    add changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_dig = number % 10 if number > 10 else number % -10
 msg = "Last digit of %d is %d " % (number, last_dig)
+if last_dig == 0:
+    print(msg, '0')
 if last_dig > 5:
-    print(msg, 'and is greater than 5')
-elif last_dig == 0:
-    print(msg, 'and is 0')
+    print(msg, 'greater than 5')
 else:
-    print(msg, 'and is less than 6 and not 0')
+    print(msg, 'less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 9):
     for number2 in range(number, 10):
-        print("{:d}{:d}".format(number, number2), end='\n' if number == 8 and number2 == 9 else ", ")
+        if number < number2:
+            print("{:d}{:d}".format(number, number2), 
+                    end='\n' if number == 8 and number2 == 9 else ", ")
