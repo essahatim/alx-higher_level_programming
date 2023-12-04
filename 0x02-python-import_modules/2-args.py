d8e6b14978e8e33db3b5fe681a27025e4f493015
essahatim <hatimessabagh@gmail.com>
2023-12-04 03:11:36 -0800

    fixed error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     size = len(sys.argv) - 1
     if size == 0:
-        print("0 arguments.".format(size))
+        print("0 arguments.")
     elif size == 1:
-        print("1 arguments.".format(size))
+        print("1 arguments.")
     else:
         print("{:d} arguments:".format(size))
     for i in range(1, size + 1):
