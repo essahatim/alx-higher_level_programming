fe7461d9ac6e87ddedee7cf3f781512079528717
essahatim <hatimessabagh@gmail.com>
2023-12-03 23:24:07 -0800

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3 
+if __name__ == "__main__":
+    import sys
+
+    size = len(sys.argv) - 1
+    if size == 0:
+        print("0 arguments.".format(size))
+    elif size > 1:
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, argv[i]))
+    else:
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, argv[1]))
