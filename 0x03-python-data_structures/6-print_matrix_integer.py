3e0c77ad191b4071d2c5e21bdb914839647b03da
essahatim <hatimessabagh@gmail.com>
2023-12-05 13:07:39 -0800

    fixed error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,11 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     else:
-        for my_list in matrix:
-            for i in matrix[my_list]:
+        for my_list in range(len(matrix)):
+            for i in range(len(matrix[my_list])):
                 if i != matrix[my_mylist] - 1:
                     space = " "
                 else:
                     space = ""
-                print(" " + "{:d}".format(matrix[my_list][i]), end=space)
+                print("{:d}".format(matrix[my_list][i]), end=space)
             print()
