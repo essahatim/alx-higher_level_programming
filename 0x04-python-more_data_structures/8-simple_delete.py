ef5086ea795da90b6a7373d5ae349c89a00718c5
essahatim <hatimessabagh@gmail.com>
2023-12-06 15:11:11 -0800

    add fils

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
 
-    if key in a_dictionary:
+
+    if a_dictionary.get(key) is not None:
         del a_dictionary[key]    
     return a_dictionary
 
