d423c215002084f3ad1f9a9ab67eca654e53c179
essahatim <hatimessabagh@gmail.com>
2023-12-08 13:14:43 -0800

    add file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+
+    if not my_list:
+        return 0
+    for score, weight in my_list:
+        t_score += score * weight
+        t_weight += weight
+
+    if t_weight == 0:
+        return 0
+    weighted_average = t_score / t_weight
+    return weighted_average
+
