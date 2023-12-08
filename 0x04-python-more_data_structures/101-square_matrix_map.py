3bfa6bf203a50de13bf25374250f1bb27b3b1344
essahatim <hatimessabagh@gmail.com>
2023-12-08 14:05:02 -0800

    add file

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
 
-    return list(map(lambda line: list(map(lambda i: i**2, line)), matrix))
+    return list(map(lambda x: list(map(lambda i: i**2, x)), matrix))
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
 
-    a_dictionary = {key: val for key,
-                    val in a_dictionary.items() if val != value}
+    my_list = {key: v for key, v in a_dictionary.items() if v != value}
+    a_dictionary = my_list
