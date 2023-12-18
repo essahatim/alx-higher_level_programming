d4eed5a07cff22baea86c7bf692c8abe5941c93e
essahatim <hatimessabagh@gmail.com>
2023-12-18 10:16:36 -0800

    add file task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        for i in range(x):
+            print(my_list=[], end=' ')
+            i += 1
+    except IndexError:
+        pass
+    print()
+    return i
