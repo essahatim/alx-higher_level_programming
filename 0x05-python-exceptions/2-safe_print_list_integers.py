d0555539a4152b646931221e1bb228e0942da05a
essahatim <hatimessabagh@gmail.com>
2023-12-18 14:51:33 -0800

    add files

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     while i < x:
         try:
             value = my_list[i]
-            print('{:d}'.format(value, end=' ')
+            print('{:d}'.format(value), end=' ')
             count += 1
         except (ValueError, TypeError):
             pass
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero")
         return None
-    else:
-        return result
     finally:
         print("Inside result: {}".format(result))
+        return result
