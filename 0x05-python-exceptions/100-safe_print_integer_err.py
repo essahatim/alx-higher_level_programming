1bb31022ff6c1e541188adf667f403d46b395a0e
essahatim <hatimessabagh@gmail.com>
2023-12-18 15:21:18 -0800

    fixed err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     an_integer = True
     try:
         print("{:d}".format(value))
-    except Exception as err:
-        print('Exception:', err, file=sys.stderr)
+    except Exception as e:
+        print('Exception:', e, file=sys.stderr)
         an_integer = False
     return an_integervalue
