a81b288f855014ab0a34e7e4d9f15e4c493d7909
essahatim <hatimessabagh@gmail.com>
2024-02-15 15:26:37 -0800

    add files

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- Create table id_not_null if it doesn't exist
+CREATE TABLE IF NOT EXISTS
+	id_not_null (
+		id INT DEFAULT 1,
+		name VARCHAR(256),
+		PRIMARY KEY (id)
+	);
+FLUSH PRIVILEGES;
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Script to create the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create table states if it doesn't exist
+CREATE TABLE IF NOT EXISTS
+	states (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(256) NOT NULL
+	);
+FLUSH PRIVILEGES;
