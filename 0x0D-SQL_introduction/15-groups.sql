6b45384ddd8bd4f6d94cedcdb32751251bb1b775
essahatim <hatimessabagh@gmail.com>
2024-02-15 11:15:36 -0800

    add files

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- List the number of records for each score
+-- Srted by the number of records (descending)
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC, score;
