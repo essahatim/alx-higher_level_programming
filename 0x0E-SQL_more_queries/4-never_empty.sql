3ad590c6b7e1ca66e143605fbbdecbe2cfa636dd
essahatim <hatimessabagh@gmail.com>
2024-02-15 16:28:21 -0800

    a

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,5 @@
 -- Script to create the table id_not_null
 -- Create table id_not_null if it doesn't exist
 CREATE TABLE IF NOT EXISTS
-	id_not_null (
-		id INT DEFAULT 1,
-		name VARCHAR(256),
-		PRIMARY KEY (id)
-	);
+	id_not_null ( id INT DEFAULT 1, name VARCHAR(256), PRIMARY KEY (id));
 FLUSH PRIVILEGES;
