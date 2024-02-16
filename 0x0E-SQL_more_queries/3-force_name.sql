ad61e888d12bb9fe9dd5a2066e7dbdb2733a1565
essahatim <hatimessabagh@gmail.com>
2024-02-15 16:17:26 -0800

    fixed error

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
 -- Create table force_name if it doesn't exist
-CREATE TABLE IF NOT EXISTS
-	force_name (id INT,  name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
-
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
 FLUSH PRIVILEGES;
