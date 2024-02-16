9f1b55cc3dce51d9fdd2dcc2b0571958269014fc
essahatim <hatimessabagh@gmail.com>
2024-02-15 16:08:58 -0800

    add files

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Script to list all shows and their genres
+-- List all shows and their genres
+USE hbtn_0d_tvshows;
+SELECT tv_shows.title, tv_genres.name
+FROM tv_showsLEFT
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
