838fc3e342d7d9a7170b26a470ac1af8b813cba2
essahatim <hatimessabagh@gmail.com>
2024-02-15 16:31:02 -0800

    a

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,6 @@
 -- Script to list all cities of California
 -- Find the state_id for California
 USE hbtn_0d_usa;
-SET @california_state_id := (
-	SELECT id FROM states WHERE name = 'California' LIMIT 1);
+SET @california_state_id := ( SELECT id FROM states WHERE name = 'California' LIMIT 1);
 SELECT * FROM cities WHERE state_id = @california_state_id
 ORDER BY id ASC;
