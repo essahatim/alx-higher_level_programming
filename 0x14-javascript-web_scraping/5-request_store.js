8c39ee8e4099054e2a3ed6b383bcf58320cd31de
essahatim <hatimessabagh@gmail.com>
2024-04-23 19:37:29 -0700

    add file & update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,22 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], (err, response, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
-    return;
-  }
-
-  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(`Content saved to ${process.argv[3]}`);
-  });
-});
+request(url).pipe(fs.createWriteStream(filePath));
