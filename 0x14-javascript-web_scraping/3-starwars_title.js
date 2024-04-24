a365c0fdb2898c02e6ba48ac375e16aedd648cdc
essahatim <hatimessabagh@gmail.com>
2024-04-23 19:01:54 -0700

    update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 
-const api_url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-request(api_url, (err, response, body) => {
+const api_Url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(api_Url, (err, response, body) => {
   if (err) {
     console.err(err);
   }
